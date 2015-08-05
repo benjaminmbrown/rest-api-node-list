900cb219e6c988f77988707d8affa983ad169ba1
Benjamin M. Brown <benjaminbb@gmail.com>
2015-08-05 15:42:53 -0500

    Modify products endpoint to return product list from db

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var restify = require('restify');
 var mongojs = require('mongojs');
 
+//replace db user/pw with your own mongolab creds
+var db = mongojs('mongodb://<dbuser>:<dbpassword>@ds031193.mongolab.com:31193/rest-api-study', ['products']);
+//for use with localdb:
+// var db = mongojs('productsdb', ['products']);
+
+
 var server = restify.createServer();
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
@@ -9,4 +15,13 @@ server.use(restify.bodyParser());
 server.listen(3000,function(){
 	console.log("Server started on port 3000");
 })
-
+//restify middleware
+server.get("/products", function(req,res,next){
+	db.products.find(function(err,products){
+		res.writeHead(200, {
+				'Content-Type': 'application/json; charseet=utf-8'
+			});
+		res.end(JSON.stringify(products));
+	});
+	return next();
+})
