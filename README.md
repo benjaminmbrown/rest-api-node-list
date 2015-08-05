48b8f3dcefe7bd0d3132668f9177cc87e060d4d4
Benjamin M. Brown <benjaminbb@gmail.com>
2015-08-05 17:30:37 -0500

    Add fetch single product endpoint. Add some sequential methods to see process

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple client/server node app to expose REST api endpoints. Results display in command line since there's no web client.
+
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var db = mongojs('mongodb://<dbuser>:<dbpassword>@ds031193.mongolab.com:31193/re
 //for use with localdb:
 // var db = mongojs('productsdb', ['products']);
 
-
 var server = restify.createServer();
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
@@ -18,7 +17,7 @@ server.listen(3000,function(){
 
 //restify middleware
 server.get("/products", function(req,res,next){
-
+	console.log('get all endpoint');
 	db.products.find(function(err,products){
 		res.writeHead(200, {
 				'Content-Type': 'application/json; charset=utf-8'
@@ -30,7 +29,7 @@ server.get("/products", function(req,res,next){
 });
 //saves product
 server.post("/product", function(req,res,next){
-
+	console.log('save (POST) endpoint');
 	var product = req.params;
 
 	db.products.save(product, function(err,data){
@@ -44,46 +43,58 @@ server.post("/product", function(req,res,next){
 });
 
 //update existing product
-server.put("/product/:id", function(res,req,next){
-	db.products.findOne(
-	{id: req.params.id},
-	 function(err,data){
+server.put('/product/:id', function (req, res, next) {
+    // get the existing product
+    db.products.findOne({
+        id: req.params.id
+    }, function (err, data) {
+        // merge req.params/product with the server/product
+ 
+        var updProd = {}; // updated products 
+        // logic similar to jQuery.extend(); to merge 2 objects.
+        for (var n in data) {
+            updProd[n] = data[n];
+        }
+        for (var n in req.params) {
+            updProd[n] = req.params[n];
+        }
+        db.products.update({
+            id: req.params.id
+        }, updProd, {
+            multi: false
+        }, function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'application/json; charset=utf-8'
+            });
+            res.end(JSON.stringify(data));
+        });
+    });
+    return next();
+});
 
-		var updatedProducts = {};
-
-		//merge 2 objects
-		for(var n in data){
-			updatedProducts[n] = data[n];
-		}
-
-		for(var n in req.params){
-			updateProducts[n] = req.params[n];
-		}
-
-		db.products.update(
-		{id: req.params.id}, 
-		updatedProducts, 
-		{multi:false},
-		function(err,data){
-			res.writeHead(200, {
-				"Content-Type" : 'application/json; charset=utf-8'
-			});
-			res.end(JSON.stringify(data));
-		});
-	});
-	return next();
+server.del('/product/:id', function (req, res, next) {
+	console.log('delete endpoint')
+    db.products.remove({
+        id: req.params.id
+    }, function (err, data) {
+    	console.log('writehead');
+        res.writeHead(200, {
+            'Content-Type': 'application/json; charset=utf-8'
+        });
+        res.end(JSON.stringify(true));
+    });
+    return next();
 });
 
 
-
-server.del('/product/:id', function (req, res, next) {
-    db.products.remove({
+server.get('/product/:id', function (req, res, next) {
+    db.products.findOne({
         id: req.params.id
     }, function (err, data) {
         res.writeHead(200, {
             'Content-Type': 'application/json; charset=utf-8'
         });
-        res.end(JSON.stringify(true));
+        res.end(JSON.stringify(data));
     });
     return next();
 });
